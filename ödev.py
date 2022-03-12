18c94e061fa3cfc97bc2aa7523b1c30ce1de55fa
Enes Gültekin <100511167+DaCaffreS@users.noreply.github.com>
2022-03-12 15:26:25 +0300

    Add files via upload

---
+++ ödev.py
@@ -0,0 +1,165 @@
+plaka=int(input("Lütfen 1'den 81'e kadar bir sayı giriniz"))
+if plaka==1:
+    print("Bu plaka Adana iline aittir")
+elif plaka==2:
+    print("Bu palaka Adıyaman iline aittir")
+elif plaka==3:
+    print("Bu plaka Afyon iline aittir")
+elif plaka==4:
+    print("Bu plaka Ağrı iline aittir ")
+elif plaka==5:
+    print("Bu plaka Amasya iline aittir")
+elif plaka==6:
+    print("Bu plaka Ankara iline aittir")
+elif plaka==7:
+    print("Bu plaka Antalya iline aittir")
+elif plaka==8:
+    print("Bu plaka Artvin iline aittir")
+elif plaka==9:
+    print("Bu plaka Aydın iline aittir")
+elif plaka==10:
+    print("Bu plaka Balıkesir iline aittir")
+elif plaka==11:
+    print("Bu plaka Bilecik iline aittir")
+elif plaka==12:
+    print("Bu plaka Bingöl iline aittir")
+elif plaka==13:
+    print("Bu plaka Bitlis iline aittir")
+elif plaka==14:
+    print("Bu plaka Bolu iline aittir ")
+elif plaka==15:
+    print("Bu plaka Burdur iline aittir ")
+elif plaka==16:
+    print("Bu plaka Bursa iline aittir ")
+elif plaka==17:
+    print("Bu plaka Çanakkale iline aittir ")
+elif plaka==18:
+    print("Bu plaka Çankırı iline aittir ")
+elif plaka==19:
+    print("Bu plaka Çorum iline aittir ")
+elif plaka==20:
+    print("Bu plaka Denizli iline aittir ")
+elif plaka==21:
+    print("Bu plaka Diyarbakır iline aittir ")
+elif plaka==22:
+    print("Bu plaka Edirne iline aittir ")
+elif plaka==23:
+    print("Bu plaka Elazığ iline aittir ")
+elif plaka==24:
+    print("Bu plaka Erzincan iline aittir ")
+elif plaka==25:
+    print("Bu plaka Erzurum iline aittir ")
+elif plaka==26:
+    print("Bu plaka Eskişehir iline aittir")
+elif plaka==27:
+    print("Bu plaka Gaziantep iline aittir")
+elif plaka==28:
+    print("Bu plaka Giresun iline aittir")
+elif plaka==29:
+    print("Bu plaka Gümüşhane iline aittir ")
+elif plaka==30:
+    print("Bu plaka Hakkari iline aittir")
+elif plaka==31:
+    print("Bu plaka Hatay iline aittir")
+elif plaka==32:
+    print("Bu plaka Isparta iline aittir")
+elif plaka==33:
+    print("Bu plaka Mersin iline aittir")
+elif plaka==34:
+    print("Bu plaka İstanbul iline aittir")
+elif plaka==35:
+    print("Bu plaka İzmir iline aittir")
+elif plaka==36:
+    print("Bu plaka Kars iline aittir")
+elif plaka==37:
+    print("Bu plaka Kastamonu iline aittir")
+elif plaka==38:
+    print("Bu plaka Kayseri iline aittir")
+elif plaka==39:
+    print("Bu plaka Kırklareli iline aittir")
+elif plaka==40:
+    print("Bu plaka Kırşehir iline aittir")
+elif plaka==41:
+    print("Bu plaka Kocaeli iline aittir")
+elif plaka==42:
+    print("Bu plaka Konya iline aittir")
+elif plaka==43:
+    print("Bu plaka Kütahya iline aittir")
+elif plaka==44:
+    print("Bu plaka Malatya iline aittir")
+elif plaka==45:
+    print("Bu plaka Manisa iline aittir")
+elif plaka==46:
+    print("Bu plaka KahramanMaraş iline aittir")
+elif plaka==47:
+    print("Bu plaka Mardin iline aittir")
+elif plaka==48:
+    print("Bu plaka Muğla iline aittir")
+elif plaka==49:
+    print("Bu plaka Muş iline aittir")
+elif plaka==50:
+    print("Bu plaka Nevşehir iline aittir")
+elif plaka==51:
+    print("Bu plaka Niğde iline aittir")
+elif plaka==52:
+    print("Bu plaka Ordu iline aittir")
+elif plaka==53:
+    print("Bu plaka Rize iline aittir")
+elif plaka==54:
+    print("Bu plaka Sakarya iline aittir")
+elif plaka==55:
+    print("Bu plaka Samsun iline aittir")
+elif plaka==56:
+    print("Bu plaka Siirt iline aittir")
+elif plaka==57:
+    print("Bu plaka Sinop iline aittir")
+elif plaka==58:
+    print("Bu plaka Sivas iline aittir")
+elif plaka==59:
+    print("Bu plaka Tekirdağ iline aittir")
+elif plaka==60:
+    print("Bu plaka Tokat iline aittir")
+elif plaka==61:
+    print("Bu plaka Trabzon iline aittir")
+elif plaka==62:
+    print("Bu plaka Tunceli iline aittir")
+elif plaka==63:
+    print("Bu plaka Şanlıurfa iline aittir")
+elif plaka==64:
+    print("Bu plaka Uşak iline aittir")
+elif plaka==65:
+    print("Bu plaka Van iline aittir")
+elif plaka==66:
+    print("Bu plaka Yozgat iline aittir")
+elif plaka==67:
+    print("Bu plaka Zonuldak iline aittir")
+elif plaka==68:
+    print("Bu plaka Aksaray iline aittir")
+elif plaka==69:
+    print("Bu plaka Bayburt iline aittir")
+elif plaka==70:
+    print("Bu plaka Karaman iline aittir")
+elif plaka==71:
+    print("Bu plaka Kırıkkale iline aittir")
+elif plaka==72:
+    print("Bu plaka Batman iline aittir")
+elif plaka==73:
+    print("Bu plaka Şırnak iline aittir")
+elif plaka==74:
+    print("Bu plaka Bartın iline aittir")
+elif plaka==75:
+    print("Bu plaka Ardahan iline aittir")
+elif plaka==76:
+    print("Bu plaka Iğdır iline aittir")
+elif plaka==77:
+    print("Bu plaka Yalova iline aittir")
+elif plaka==78:
+    print("Bu plaka Karabük iline aittir ")
+elif plaka==79:
+    print("Bu plaka Kilis iline aittir")
+elif plaka==80:
+    print("Bu plaka Osmaniye iline aittir")
+elif plaka==81:
+    print("Bu plaka Düzce iline aittir")
+else:
+    print("Girilen sayıda bir plaka bulunamadı.")
